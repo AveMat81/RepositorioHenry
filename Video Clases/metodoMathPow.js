e205140bea526d98de33bed6d32c840754ea3983
AveMat81 <matiasavesani@yahoo.com.ar>
2023-03-27 13:53:41 -0300

    Metodo Math.pow

---
+++ Video Clases/metodoMathPow.js
@@ -0,0 +1,35 @@
+//METODOS MATH.
+
+//Math.pow
+
+Math.pow(2, 3) //2 elevado al cubo
+//8
+
+
+//Math.round & Math.floor & Math.ceil
+
+Math.round(0.49)
+//0
+
+Math.round(0.50)
+//1
+
+Math.floor(0.50)
+//1
+
+Math.ceil(3.27)
+//4
+
+
+//Math.max & Math.min
+
+Math.max(1, 2, 3, 4, 5)
+//5
+
+Math.min(1, 2, 3, 4, 5)
+//1
+
+
+//Math.random   (MUESTRA UN NÚMERO ALEATARIO CUALQUIERA ENTRE EL 0 Y 1)
+Math.random()
+//0.8966554
