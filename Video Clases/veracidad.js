ee49dca2a3a958a4b812f5d8d30945b462b3c50a
AveMat81 <matiasavesani@yahoo.com.ar>
2023-03-27 16:08:24 -0300

    Veracidad

---
+++ Video Clases/veracidad.js
@@ -0,0 +1,11 @@
+//Utiliza la función Boolean() e ingresa cualquiera de los siguientes valores 
+//para comprobar cuál es su veracidad:
+1           // true
+0           // false
+-1          // true
+true        // true
+false       // false
+'string'    // true
+null        // false
+undefined   // false
+[]          // true
