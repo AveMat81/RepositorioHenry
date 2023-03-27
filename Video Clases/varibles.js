d323ae0111d84577bceb73cd96ef9f92398ccb72
AveMat81 <matiasavesani@yahoo.com.ar>
2023-03-27 11:57:46 -0300

    Video clases hasta funciones

---
+++ Video Clases/varibles.js
@@ -0,0 +1,29 @@
+//String
+
+var bootcamp = "Henry";
+
+//Números
+
+var numeroEntero = 17;
+
+var numeroNegativo = -4;
+
+var numeroDecimal = 11.75;
+
+//Booleanos
+
+var estoyEntendiendo = true;
+
+var estoyAburrido = false;
+
+//Undefined
+var cajón1;
+console.log(cajon1);
+
+//Variable que no ah sido declarada o a la cual no se le ah asignado un valor.
+
+
+//Null
+var sinNada = nulo;
+
+//Ausencia intencional de cualquier valor, valor nulo o vacio.
