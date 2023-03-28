91ba8499f949c0aa0e375263adfd2998b7fd86b9
AveMat81 <matiasavesani@yahoo.com.ar>
2023-03-28 17:21:43 -0300

    Bules

---
+++ Video Clases/bucles.js
@@ -0,0 +1,121 @@
+// **Bucle for **
+
+var suma = 0
+
+// for (var i=0; i<5; i++){
+//     suma = suma + 1;
+//     console.log(suma)
+// }
+
+// for (var i=0; i<5; i++){
+//     suma = suma + i;
+//     console.log("Variable de iteracion i: ", i);
+// }
+// console.log("Variavble suma", suma);
+
+// for (var i=0; i<5; i++){
+//     suma = suma + i;
+//     console.log(suma);
+// }
+
+
+// **Bucle while**
+
+// while (suma < 5){
+//     suma = suma + 1;
+//     console.log(suma)
+// }
+
+
+// **do while**
+
+// let result = '';
+// let i = 0;
+
+// do {
+//   i = i + 1;
+//   result = result + i;
+// } while (i < 5);
+
+// console.log(result);
+// Expected output: "12345"
+
+let result = 0;
+let i = 0;
+
+do {
+  i = i + 1;
+  result = result + i;
+} while (i < 5);
+
+console.log(result);
+// Expected output: 15
+
+
+// // Expresion switch
+
+// switch (expresión) {
+//     case valor1:
+//       //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+//       [break;]
+//     case valor2:
+//       //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor2
+//       [break;]
+//     ...
+//     case valorN:
+//       //Declaraciones ejecutadas cuando el resultado de expresión coincide con valorN
+//       [break;]
+//     default:
+//       //Declaraciones ejecutadas cuando ninguno de los valores coincide con el valor de la expresión
+//       [break;]
+//   }
+
+// 	var mes = 0
+// 	switch (mes){
+// 		case 1:
+// 			 console.log("Enero")
+//            break
+// 		case 2:
+// 			console.log("Febrero")
+//             break
+// 		case 3:
+// 			console.log("Marzo")
+//             break
+// 		case 4:
+// 			console.log("Abril")
+//             break
+// 		case 5:
+// 			console.log("Mayo")
+//             break
+// 		case 6:
+// 			console.log("Junio")
+//             break
+// 		case 7:
+// 			console.log("Julio")
+//             break
+// 		case 8:
+// 			console.log("Agosto")
+//             break
+// 		case 9:
+// 			console.log("Septiembre")
+//             break
+// 		case 10:
+// 			console.log("Octubre")
+//             break
+// 		case 11:
+// 			console.log("Noviembre")
+//             break
+// 		case 12:
+// 			console.log("Diciembre")
+//             break
+// 	    default:
+//         console.log("Mensaje de error! El número seleccionado no corresponde a un mes del año")
+//             break
+// }
+
+// **Declaración Continue**
+
+// La declaración continue se utiliza dentro de los Bucles For. Nos permite omitir alguna de las iteraciones si se cumple una condición específica.
+
+// **Break**
+// La declaración break se utiliza dentro de los Bucles For. Nos permite "romper" o finalizar el bucle con antelación si se cumple una condición específica.
